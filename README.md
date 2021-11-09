c515525621bbda9089bdad25b760fb16fa27fe59
Luj8n <l.milieska@gmail.com>
2021-11-09 17:50:29 +0200

    many changes including caching

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # TODO:
 
 - ### Fix the naming convention
-- ### Add specific functions just to call the piston api
 - ### Add better error info
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use cached::proc_macro::cached;
 use itertools::Itertools;
 use rocket::{
   response::status,
@@ -22,23 +23,8 @@ const RUN_MEMORY_LIMIT: i64 = 512 * 1024 * 1024;
 const EXECUTE_API: &str = "http://localhost:2000/api/v2/execute";
 const RUNTIMES_API: &str = "http://localhost:2000/api/v2/runtimes";
 
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct ExecuteRequest {
-  language: String,
-  version: String,
-  code: String,
-  test: Test,
-  timeout: i64,
-}
-
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct Test {
-  input: String,
-  output: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct RunJson {
+#[derive(Serialize, Deserialize)]
+struct PistonRun {
   stdout: String,
   stderr: String,
   code: Option<i64>,
@@ -46,38 +32,24 @@ struct RunJson {
   output: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct ExecuteJson {
-  run: RunJson,
+#[derive(Serialize, Deserialize)]
+struct PistonExecution {
+  run: PistonRun,
   language: String,
   version: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct RuntimeReturnJson {
-  language: String,
-  version: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct RuntimeJson {
-  language: String,
-  version: String,
-  aliases: Vec<String>,
-  runtime: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct FileJson {
+#[derive(Serialize, Deserialize)]
+struct PistonFile {
   name: Option<String>,
   content: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct PostExecuteJson {
+#[derive(Serialize, Deserialize)]
+struct PistonExecuteRequest {
   language: String,
   version: String,
-  files: Vec<FileJson>,
+  files: Vec<PistonFile>,
   stdin: Option<String>,
   args: Option<Vec<String>>,
   compile_timeout: Option<i64>,
@@ -86,27 +58,58 @@ struct PostExecuteJson {
   run_memory_limit: Option<i64>,
 }
 
-#[derive(Serialize, Deserialize, Debug, JsonSchema)]
-struct ExecuteResult {
+#[derive(Serialize, Deserialize)]
+struct PistonRuntime {
+  language: String,
+  version: String,
+  aliases: Vec<String>,
+  runtime: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Hash, Eq, PartialEq)]
+struct ExecuteCodeRequest {
+  code: String,
+  language: String,
+  stdin: Option<String>,
+  timeout: Option<i64>,
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Clone)]
+struct Execution {
   stdout: String,
   stderr: Option<String>,
-  passed: bool,
   time_limit_exceeded: bool,
 }
 
-#[openapi]
-#[post("/submit", data = "<data>")]
-async fn submit(data: Json<ExecuteRequest>) -> Result<Json<ExecuteResult>, status::NotFound<String>> {
-  let execute_json = PostExecuteJson {
+#[derive(Serialize, Deserialize, JsonSchema, Clone)]
+struct Runtime {
+  language: String,
+  version: String,
+}
+
+#[cached(time = 60, result = true)]
+async fn piston_execute(data: ExecuteCodeRequest) -> Result<Execution, String> {
+  if let Some(timeout) = data.timeout {
+    if !(1..=3000).contains(&timeout) {
+      return Err("Timeout must be between 1 and 3000 ms (inclusive)".to_string());
+    }
+  }
+
+  let execute_json = PistonExecuteRequest {
     language: data.language.to_owned(),
-    version: data.version.to_owned(),
+    version: piston_runtimes()
+      .await?
+      .into_iter()
+      .find(|runtime| runtime.language == data.language)
+      .ok_or(format!("Couldn't find '{}' language", data.language))?
+      .version,
     args: None,
     compile_memory_limit: Some(COMPILE_MEMORY_LIMIT),
     compile_timeout: None,
     run_memory_limit: Some(RUN_MEMORY_LIMIT),
-    run_timeout: Some(data.timeout),
-    stdin: Some(data.test.input.to_owned()),
-    files: vec![FileJson {
+    run_timeout: data.timeout,
+    stdin: data.stdin,
+    files: vec![PistonFile {
       name: None,
       content: data.code.to_owned(),
     }],
@@ -117,10 +120,10 @@ async fn submit(data: Json<ExecuteRequest>) -> Result<Json<ExecuteResult>, statu
     .json(&execute_json)
     .send()
     .await
-    .map_err(|e| status::NotFound(e.to_string()))?
-    .json::<ExecuteJson>()
+    .map_err(|e| e.to_string())?
+    .json::<PistonExecution>()
     .await
-    .map_err(|e| status::NotFound(e.to_string()))?;
+    .map_err(|e| e.to_string())?;
 
   let stdout = if res.run.stdout.ends_with('\n') {
     // chop off \n if it ends with it
@@ -131,35 +134,46 @@ async fn submit(data: Json<ExecuteRequest>) -> Result<Json<ExecuteResult>, statu
 
   let time_limit_exceeded = res.run.signal.map(|s| s == "SIGKILL").unwrap_or(false);
 
-  Ok(Json(ExecuteResult {
-    stdout: res.run.stdout.to_owned(),
+  Ok(Execution {
+    stdout,
     stderr: if res.run.stderr.is_empty() {
       None
     } else {
       Some(res.run.stderr)
     },
-    passed: stdout == data.test.output && !time_limit_exceeded,
     time_limit_exceeded,
-  }))
+  })
 }
 
 #[openapi]
-#[get("/runtimes")]
-async fn runtimes() -> Result<Json<Vec<RuntimeReturnJson>>, status::NotFound<String>> {
+#[post("/execute", data = "<data>")]
+async fn execute(data: Json<ExecuteCodeRequest>) -> Result<Json<Execution>, status::NotFound<String>> {
+  piston_execute(data.clone()).await.map(Json).map_err(status::NotFound)
+}
+
+#[cached(time = 60, result = true)]
+async fn piston_runtimes() -> Result<Vec<Runtime>, String> {
+  println!("FETCHING");
   let res = reqwest::get(RUNTIMES_API)
     .await
-    .map_err(|e| status::NotFound(e.to_string()))?
-    .json::<Vec<RuntimeJson>>()
+    .map_err(|e| e.to_string())?
+    .json::<Vec<PistonRuntime>>()
     .await
-    .map_err(|e| status::NotFound(e.to_string()))?
+    .map_err(|e| e.to_string())?
     .iter()
-    .map(|r| RuntimeReturnJson {
+    .map(|r| Runtime {
       language: r.language.to_owned(),
       version: r.version.to_owned(),
     })
     .collect_vec();
 
-  Ok(Json(res))
+  Ok(res)
+}
+
+#[openapi]
+#[get("/runtimes")]
+async fn runtimes() -> Result<Json<Vec<Runtime>>, status::NotFound<String>> {
+  piston_runtimes().await.map(Json).map_err(status::NotFound)
 }
 
 #[launch]
@@ -168,7 +182,7 @@ fn rocket() -> _ {
     port: PORT,
     ..Config::default()
   })
-  .mount("/", openapi_get_routes![runtimes, submit])
+  .mount("/", openapi_get_routes![runtimes, execute])
   .mount(
     "/swagger-ui/",
     swagger_ui::make_swagger_ui(&swagger_ui::SwaggerUIConfig {
